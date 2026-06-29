4fca841d636ffddf22a5136c25fe2e035882306b
Tentchou <tentchouromeo58@gmail.com>
2026-06-29 15:16:01 +0100

    ajout des variables d'environements

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,11 +1,13 @@
+
 import axios from 'axios';
 
-// On pointe vers l'URL de notre API .NET locale
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5109/api',
+  // Si VITE_API_URL existe (en prod), il l'utilise. Sinon, il prend le localhost (en dev).
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:7000/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
+// ... (le reste de ton code pour injecter le token Clerk si tu l'as configuré)
 export default axiosClient;
